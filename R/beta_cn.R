e4d88a9c19e89316191d0860e34a33503fe07a7f
koyelucd <94002124+koyelucd@users.noreply.github.com>
2021-11-09 16:51:39 +0000

    Update beta_cn.R
    if condition changed for ncores

--- R/beta_cn.R
+++ R/beta_cn.R
@@ -14,7 +14,7 @@ beta_cn<-function(X,K=3,seed,register=NULL){
     set.seed(seed)
 
   ## select the # of cores on which the parallel code is to run
-  if(!is.null(register)){
+  if(is.null(register)){
     ncores = parallel::detectCores()
     doParallel::registerDoParallel(ncores-1)}
 
