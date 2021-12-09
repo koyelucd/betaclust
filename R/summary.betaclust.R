b27514b938ab85ec99b00adfc226bd50d983742c
koyelucd <94002124+koyelucd@users.noreply.github.com>
2021-12-09 13:28:15 +0000

    summary updated

--- R/summary.betaclust.R
+++ R/summary.betaclust.R
@@ -5,15 +5,21 @@
 
 summary.betaclust<-function(object)
 {
-  K<-object$K
+  #K<-object$K
+  C=nrow(object$best_model$data)
+  d=ncol(object$best_model$data)
   title <- paste("Multivariate Beta mixture model fitted by EM algorithm")
+  ic_value=min(object$ic_op)
+  clust_count=length(object$best_model$cluster_count)
+  loglik=object$best_model$llk[length(object$best_model$llk)]
   obj <- list(title = title,
-              C = nrow(object$best_model$data),
-              d = ncol(object$best_model$data),
-              cluster_count = length(object$best_model$cluster_count),
+              C = C,
+              d = d,
+              cluster_count = clust_count,
               modelName = object$optimal_model,
-              loglik = object$best_model$llk[length(object$best_model$llk)],
-              information_criterion=object$model_selection,ic_output=min(object$ic_op))
+              loglik = loglik,
+              information_criterion=object$model_selection,
+              ic_output=ic_value)
   class(obj) <- "summary.betaclust"
   return(obj)
 }
