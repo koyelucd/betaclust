6b4f1c93d00ae0bafb9a5952a2fd19bbd1051673
koyelucd <94002124+koyelucd@users.noreply.github.com>
2022-03-09 11:51:18 +0000

    changes in C.. prop displayed in graph

--- R/plot.betaclust.R
+++ R/plot.betaclust.R
@@ -34,12 +34,12 @@ plot.betaclust <- function(object,what="density",
           })
           p.text <- do.call(rbind, p.text)
           p.text$prop=p.text$n/(sum(p.text$n))
-          p.text$prop=round(p.text$prop,2)
+          p.text$prop=round(p.text$prop,3)
 
 
           plot_graph<-plot_graph + ggplot2::annotate('text', x = p.text$x,
                                                      y = p.text$y,
-                                                     label = sprintf('%f',
+                                                     label = sprintf('%.3f',
                                                                      p.text$prop),
                                                      vjust = 0)
 
