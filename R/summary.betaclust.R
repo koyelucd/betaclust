792191478367926dfd14688b39beb4450687c9fc
koyelucd <94002124+koyelucd@users.noreply.github.com>
2022-06-15 11:14:21 +0100

    Update summary.betaclust.R

--- R/summary.betaclust.R
+++ R/summary.betaclust.R
@@ -40,40 +40,7 @@ summary.betaclust<-function(object)
               ic_output=ic_value,
               classification=classification)
   class(obj) <- "summary.betaclust"
+  print(obj)
   return(obj)
 }
 
-print.summary.betaclust <- function(x, digits = getOption("digits"), ...)
-{
-  txt <- paste(rep("-", min(nchar(x$title), getOption("width"))), collapse = "")
-  cat(txt)
-  cat("\n")
-  cat(x$title)
-  cat("\n")
-  cat(txt)
-  #
-  cat("\n")
-  if(x$K == 0)
-  {
-    cat("betaclust model with only a noise component:")
-  } else
-  {
-    cat(paste0("betaclust ", x$modelName, " model with ",
-               x$K, ifelse(x$K > 1, " components", " component"),
-               ":"))
-  }
-  cat("\n")
-
-
-
-  tab <- data.frame("log-likelihood" = x$loglik,  "Information-criterion"=x$information_criterion,"IC-value" = x$ic_output,
-                    "CpG-sites" = x$C, "Patients" = x$N, "Samples" = x$R,
-                    row.names = "", check.names = FALSE)
-  print(tab)
-  #
-  cat("\nClustering table:")
-  #print(table(x$classification))
-  print(x$classification)
-  #
-  invisible(x)
-}
